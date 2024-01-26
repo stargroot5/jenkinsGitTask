65551af281181c91172bba44ecea54e29d344194
stargroot5 <iharel08@gmail.com>
2024-01-26 12:02:57 +0200

    changed the bye to the virus

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         for( int i=1; i<=10;i++){
-            System.out.println(i+": bye, have a good time!");
+            System.out.println(i+": your computer has virus!");
         }
     }
 }
