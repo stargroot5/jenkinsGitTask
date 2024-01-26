9fc464404d80ed8557b3ebd1cec2aae97237d836
stargroot5 <iharel08@gmail.com>
2024-01-26 11:59:16 +0200

    changed the name to we will win

--- Idan.java
+++ Idan.java
@@ -1,7 +1,7 @@
 public class Idan{
     public static void main(String[] args){
         for( int i=1; i<=10;i++){
-            System.out.println("Idan Harel");
+            System.out.println("We will win!!!!!");
         }
     }
 }
